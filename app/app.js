f1ced68a3180b3b0e5c0e3428af95a3e43d1bade
student <michal.niedziela@labnoratory.com>
2017-01-17 12:17:45 +0100

    Create post

--- app/app.js
+++ app/app.js
@@ -7,6 +7,41 @@ var db = require('./db');
 module.exports = function createApp() {
 
     var app = express();
+    var validToken = new Date().getTime().toString();
+    authenticator.setValidToken(validToken);
+    app.use(function(req, res, next){
+        var data = "";
+        req.on('data', function(chunk){ data += chunk});
+        req.on('end', function(){
+            req.rawBody = data;
+            req.jsonBody = JSON.parse(data);
+            next();
+        })
+    });
+
+    app.use(function(req,res,next){
+        res.header('authorization', 'Bearer '+ validToken);
+        next();
+    });
+
+    app.post('/dog', function(req,res){
+        var data = req.jsonBody;
+        db.connect();
+        var result = db.save('dog',data).then(function (data)
+        {
+            return data;
+        });
+        db.disconnect();
+
+        res.status(201).json(result);
+    });
+
+    app.get('/', function (req, res)
+    {
+        res.end('Hello');
+    });
+
+
 
     return app;
 };
