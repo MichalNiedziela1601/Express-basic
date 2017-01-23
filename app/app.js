8a30b1054b689efce42891e8ec6c689916d819ff
student <michal.niedziela@labnoratory.com>
2017-01-23 11:12:15 +0100

    Check post data

--- app/app.js
+++ app/app.js
@@ -4,43 +4,69 @@ var express = require('express');
 var authenticator = require('./authenticator');
 var db = require('./db');
 
-module.exports = function createApp() {
+module.exports = function createApp()
+{
 
     var app = express();
-    var validToken = new Date().getTime().toString();
-    authenticator.setValidToken(validToken);
-    app.use(function(req, res, next){
+    // var validToken = new Date().getTime().toString();
+    var postLength = 0;
+    // authenticator.setValidToken(validToken);
+    app.use(function (req, res, next)
+    {
         var data = "";
-        req.on('data', function(chunk){ data += chunk});
-        req.on('end', function(){
-            req.rawBody = data;
+        req.on('data', function (chunk)
+        {
+            data += chunk;
+        });
+        req.on('end', function ()
+        {
             req.jsonBody = JSON.parse(data);
             next();
         })
     });
 
-    app.use(function(req,res,next){
-        res.header('authorization', 'Bearer '+ validToken);
-        next();
+    app.use(function (req, res, next)
+    {
+        if (authenticator.authenticate(parseInt(req.header('authorization'),10))) {
+            res.header('authorization', 'Bearer ' + parseInt(req.header('authorization'),10));
+            next();
+        }
+        else {
+            res.status(401);
+            res.end();
+        }
     });
 
-    app.post('/dog', function(req,res){
-        var data = req.jsonBody;
-        db.connect();
-        var result = db.save('dog',data).then(function (data)
-        {
-            return data;
-        });
-        db.disconnect();
+    app.post('/dog', function (req, res)
+    {
 
-        res.status(201).json(result);
+        var data = req.jsonBody;
+        if(null == data.name || data.name.length == 0){
+            res.status(400);
+            res.end();
+        } else {
+            db.connect();
+            db.save('dog', data).then(function (result)
+            {
+                res.status(200).send(result);
+            })
+                    .catch(function (err)
+                    {
+                        console.log(err);
+                    });
+
+            db.disconnect();
+        }
+    });
+
+    app.get('/dog', function(req,res){
+
     });
 
     app.get('/', function (req, res)
     {
         res.end('Hello');
     });
-
 
 
     return app;
--- app/server.js
+++ app/server.js
@@ -1,3 +1,4 @@
 var app = require('./app');
 
 app().listen(3000);
+console.log('Server start on port 3000');
