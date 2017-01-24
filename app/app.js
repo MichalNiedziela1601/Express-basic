7a1eb8f77a52b4619efb4ad1c86e05f03eb5ecff
student <michal.niedziela@labnoratory.com>
2017-01-24 08:55:28 +0100

    Test task1

--- app/app.js
+++ app/app.js
@@ -8,65 +8,112 @@ module.exports = function createApp()
 {
 
     var app = express();
-    // var validToken = new Date().getTime().toString();
-    var postLength = 0;
-    // authenticator.setValidToken(validToken);
-    app.use(function (req, res, next)
-    {
-        var data = "";
-        req.on('data', function (chunk)
-        {
-            data += chunk;
-        });
-        req.on('end', function ()
-        {
-            req.jsonBody = JSON.parse(data);
-            next();
-        })
-    });
+    // db.connect();
+    // authenticator.setValidToken('12');
 
+    function byteCount(s)
+    {
+        return encodeURI(s).split(/%..|./).length - 1;
+    }
+
+    app.use(express.static('public'));
     app.use(function (req, res, next)
     {
-        if (authenticator.authenticate(parseInt(req.header('authorization'),10))) {
-            res.header('authorization', 'Bearer ' + parseInt(req.header('authorization'),10));
-            next();
+        var tHeader = req.header('authorization');
+        if (tHeader) {
+            var token = tHeader.substr(7);
         }
-        else {
+        if (authenticator.authenticate(token)) {
+            res.header('authorization', 'Bearer ' + token);
+            next();
+        } else {
             res.status(401);
             res.end();
         }
     });
 
+    app.use(function (req, res, next)
+    {
+        if (req.method === 'GET') {
+            next();
+        } else {
+            var data = "";
+            req.on('data', function (chunk)
+            {
+                data += chunk;
+            });
+
+            req.on('end', function ()
+            {
+                if (byteCount(data) > 70) {
+                    res.sendStatus(413);
+                } else {
+                    req.jsonBody = JSON.parse(data);
+                    next();
+                }
+            });
+        }
+
+    });
+
+
     app.post('/dog', function (req, res)
     {
 
         var data = req.jsonBody;
-        if(null == data.name || data.name.length == 0){
+        if (null == data.name || data.name.length == 0 || data.name.length > 10 || typeof data.owner === 'number' || data.createDate < 0) {
             res.status(400);
             res.end();
         } else {
-            db.connect();
+
             db.save('dog', data).then(function (result)
             {
                 res.status(200).send(result);
             })
                     .catch(function (err)
                     {
-                        console.log(err);
+                        if (err === 'No connection') {
+                            res.status(500);
+                            res.end();
+                        } else if (err === 'Entity not found') {
+                            res.status(404);
+                            res.end();
+                        }
                     });
 
-            db.disconnect();
         }
     });
 
-    app.get('/dog', function(req,res){
 
-    });
-
-    app.get('/', function (req, res)
+    app.get('/dog/:id', function (req, res)
     {
-        res.end('Hello');
+        var id = req.params.id;
+        db.get('dog', id)
+                .then(function (result)
+                {
+                    res.status(200).send(result)
+                })
+                .catch(function (err)
+                {
+                    if (err === 'No connection') {
+                        res.status(500);
+                        res.end();
+                    } else if (err === 'Entity not found') {
+                        res.status(404);
+                        res.end();
+                    }
+                });
     });
+
+    app.get('/hound/:id', function (req, res)
+    {
+        var id = req.params.id;
+        res.statusCode = 302;
+        res.setHeader('Location', '/dog/' + id);
+        res.end();
+
+    });
+
 
 
     return app;
--- app/server.js
+++
@@ -1,4 +0,0 @@
-var app = require('./app');
-
-app().listen(3000);
-console.log('Server start on port 3000');
